bdebaeab991e2e8329bfab830ab66db54f5736a3
이명진 <happy_9586@naver.com>
2025-10-24 13:10:14 +0900

    Firebase config settings

---
+++ Assets/Scripts/FirebaseInitializer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Firebase;
+using Cysharp.Threading.Tasks;
+
+public class FirebaseInitializer : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+
+    }
+}
